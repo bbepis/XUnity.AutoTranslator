5bcc3e0a73817b85e2da436551068e645a06fbbf
Scrublord1336 <41587070+Scrublord1336@users.noreply.github.com>
2018-07-26 17:06:37 +0200

    initial unity injector implementation

---
+++ src/XUnity.AutoTranslator.Plugin.UnityInjector/AutoTranslatorPlugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExIni;
+using UnityInjector;
+using UnityInjector.Attributes;
+using XUnity.AutoTranslator.Plugin.Core;
+using XUnity.AutoTranslator.Plugin.Core.Configuration;
+
+namespace XUnity.AutoTranslator.Plugin.UnityInjector
+{
+   [PluginName( "XUnity.AutoTranslator" ), PluginVersion( "2.6.0" )]
+   public class AutoTranslatorPlugin : PluginBase, IConfiguration
+   {
+      IniFile IConfiguration.ReloadConfig()
+      {
+         return ReloadConfig();
+      }
+
+      void IConfiguration.SaveConfig()
+      {
+         SaveConfig();
+      }
+
+      public void Awake()
+      {
+         PluginLoader.LoadWithConfig( this );
+      }
+   }
+}
