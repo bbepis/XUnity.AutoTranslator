39582984c065abaf63b84339f97499ff35f400db
Scrublord1336 <41587070+Scrublord1336@users.noreply.github.com>
2018-08-07 20:29:08 +0200

    fix build error

--- src/XUnity.AutoTranslator.Plugin.Core/Extensions/ObjectExtensions.cs
+++ src/XUnity.AutoTranslator.Plugin.Core/Extensions/ObjectExtensions.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using UnityEngine;
 using XUnity.AutoTranslator.Plugin.Core.Configuration;
 using UnityEngine.UI;
 using XUnity.AutoTranslator.Plugin.Core.Constants;
