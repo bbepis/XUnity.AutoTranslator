a72f2e621e2f98fe6186e3a153b6cf7bd8cc5f4e
randoman <738b86bb93c44695854182cc459afcbb@lonestar.no>
2019-08-24 21:32:59 +0200

    added koikatsu-specific solution

---
+++ src/XUnity.AutoTranslator.KoikatsuResources/KoikatsuTextResourceRedirectorPlugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+
+namespace KoikatsuTextResourceRedirector
+{
+    [BepInDependency("gravydevsupreme.xunity.autotranslator", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(GUID: "gravydevsupreme.koikatsu.textresourceredirector", Name: "KoikatsuTextResourceRedirector", Version: "1.0.0")]
+    public class KoikatsuTextResourceRedirectorPlugin : BaseUnityPlugin
+    {
+        private ExcelDataResourceRedirector _excelRedirector;
+        private ScenarioDataResourceRedirector _scenarioRedirector;
+
+        void Awake()
+        {
+            _excelRedirector = new ExcelDataResourceRedirector();
+            _scenarioRedirector = new ScenarioDataResourceRedirector();
+
+            // disable thy self!
+            enabled = false;
+        }
+    }
+}
---
+++ src/XUnity.AutoTranslator.KoikatsuResources/ScenarioDataResourceRedirector.cs
@@ -0,0 +1,76 @@
+﻿using ADV;
+using BepInEx;
+using System.IO;
+using XUnity.AutoTranslator.Plugin.Core;
+using XUnity.AutoTranslator.Plugin.Core.AssetRedirection;
+using XUnity.AutoTranslator.Plugin.Core.Utilities;
+using XUnity.ResourceRedirector;
+
+namespace KoikatsuTextResourceRedirector
+{
+    public class ScenarioDataResourceRedirector : AssetLoadedHandlerBase<ScenarioData>
+    {
+        protected override string CalculateModificationFilePath(ScenarioData asset, IAssetOrResourceLoadedContext context)
+        {
+            return context.GetPreferredFilePathWithCustomFileName(@"BepInEx\translation", asset, "translation.txt")
+                .Replace(".unity3d", "");
+        }
+
+        protected override bool DumpAsset(string calculatedModificationPath, ScenarioData asset, IAssetOrResourceLoadedContext context)
+        {
+            var cache = new SimpleTextTranslationCache(calculatedModificationPath, false);
+
+            foreach (var param in asset.list)
+            {
+                if (param.Command == Command.Text)
+                {
+                    for (int i = 0; i < param.Args.Length; i++)
+                    {
+                        var key = param.Args[i];
+
+                        if (!string.IsNullOrEmpty(key) && LanguageHelper.IsTranslatable(key))
+                        {
+                            cache.AddTranslationToCache(key, key);
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        protected override bool ReplaceOrUpdateAsset(string calculatedModificationPath, ref ScenarioData asset, IAssetOrResourceLoadedContext context)
+        {
+            var cache = new SimpleTextTranslationCache(calculatedModificationPath, true);
+
+            foreach (var param in asset.list)
+            {
+                if (param.Command == Command.Text)
+                {
+                    for (int i = 0; i < param.Args.Length; i++)
+                    {
+                        var key = param.Args[i];
+                        if (!string.IsNullOrEmpty(key))
+                        {
+                            if (cache.TryGetTranslation(key, true, out var translated))
+                            {
+                                param.Args[i] = translated;
+                            }
+                            else if (IsDumpingEnabled && LanguageHelper.IsTranslatable(key))
+                            {
+                                cache.AddTranslationToCache(key, key);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        protected override bool ShouldHandleAsset(ScenarioData asset, IAssetOrResourceLoadedContext context)
+        {
+            return !context.HasReferenceBeenRedirectedBefore(asset);
+        }
+    }
+}
