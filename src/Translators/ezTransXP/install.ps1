474587890ac6fb25b72a76474a94760d3f54ae16
Seniorious <wldnjs1323@naver.com>
2020-03-12 13:23:14 +0900

    build setting changed

--- src/Translators/ezTransXP/install.ps1
+++
@@ -1,9 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-$asms = $package.AssemblyReferences | %{$_.Name} 
-foreach ($reference in $project.Object.References) 
-{
-    if ($asms -contains $reference.Name + ".dll") 
-    {
-        $reference.CopyLocal = $false;
-    }
-}
