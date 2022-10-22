c332e35b2d81e3216011c9a00c0feb11573b76a1
randoman <738b86bb93c44695854182cc459afcbb@lonestar.no>
2022-10-22 23:02:33 +0200

    ### 5.2.0
     * FEATURE - Added support for Lingo Cloud - thanks to Kiles Duli
     * BUG FIX - Fixed IL2CPP issue where plugin did not always use correct unhollowed paths for BepInEx

--- src/Translators/DeepLTranslate.ExtProtocol/ExtDeepLTranslateLegitimate.cs
+++ src/Translators/DeepLTranslate.ExtProtocol/ExtDeepLTranslateLegitimate.cs
@@ -93,7 +93,7 @@ namespace DeepLTranslate.ExtProtocol
          _handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
 
          _client = new HttpClient( _handler, true );
-         _client.DefaultRequestHeaders.UserAgent.Add( new ProductInfoHeaderValue( "XUnity", "5.1.0" ) );
+         _client.DefaultRequestHeaders.UserAgent.Add( new ProductInfoHeaderValue( "XUnity", "5.2.0" ) );
          _client.DefaultRequestHeaders.Accept.Add( new MediaTypeWithQualityHeaderValue( "*/*" ) );
       }
 
--- src/XUnity.AutoTranslator.Patcher/Patcher.cs
+++ src/XUnity.AutoTranslator.Patcher/Patcher.cs
@@ -29,7 +29,7 @@ namespace XUnity.AutoTranslator.Patcher
       {
          get
          {
-            return "5.1.0";
+            return "5.2.0";
          }
       }
 
