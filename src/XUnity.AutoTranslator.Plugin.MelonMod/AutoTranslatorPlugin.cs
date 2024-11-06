461a0b3206bc78d4c43c382e740aced5cacfc87f
PranitModak <43670565+PranitModak@users.noreply.github.com>
2024-11-06 15:31:53 +0530

    Update AutoTranslatorPlugin.cs
    add configuration capability in android.
    Moving config location to designated Melon directory

--- src/XUnity.AutoTranslator.Plugin.MelonMod/AutoTranslatorPlugin.cs
+++ src/XUnity.AutoTranslator.Plugin.MelonMod/AutoTranslatorPlugin.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,8 +19,11 @@ using XUnity.Common.Logging;
 
 namespace XUnity.AutoTranslator.Plugin.MelonMod
 {
-   public class AutoTranslatorPlugin : MelonLoader.MelonMod
+   public class AutoTranslatorPlugin : MelonLoader.MelonMod, IPluginEnvironment
    {
+      private ExIni.IniFile _file;
+      private string _configPath;
+      private string _dataFolder;
       public override void OnApplicationLateStart()
       {
 #if IL2CPP
@@ -28,9 +31,46 @@ namespace XUnity.AutoTranslator.Plugin.MelonMod
          var gameDir = modFi.Directory.Parent;
          var unhollowedPath = Path.Combine( gameDir.FullName, "MelonLoader", "Il2CppAssemblies" );
          Il2CppProxyAssemblies.Location = unhollowedPath;
+         _dataFolder = Path.Combine( gameDir.FullName, "AutoTranslator" );
+         _configPath = Path.Combine( _dataFolder, "Config.ini" );
+         TranslationPath =_dataFolder;
 #endif
 
-         PluginLoader.Load( false );
+         PluginLoader.LoadWithConfig( this );
+      }
+
+      public ExIni.IniFile Preferences
+      {
+         get
+         {
+            return ( _file ?? ( _file = ReloadConfig() ) );
+         }
+      }
+
+      public string ConfigPath { get; }
+
+      public string TranslationPath { get; set; }
+
+      public bool AllowDefaultInitializeHarmonyDetourBridge => false;
+
+      public ExIni.IniFile ReloadConfig()
+      {
+         if( !File.Exists( _configPath ) )
+         {
+            return ( _file ?? new ExIni.IniFile() );
+         }
+         ExIni.IniFile ini = ExIni.IniFile.FromFile( _configPath );
+         if( _file == null )
+         {
+            return ( _file = ini );
+         }
+         _file.Merge( ini );
+         return _file;
+      }
+
+      public void SaveConfig()
+      {
+         _file.Save( _configPath );
       }
    }
 }
