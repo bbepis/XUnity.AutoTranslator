093573d36df76f27be11ef4d0b0316f67ffb317a
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-01-31 15:15:22 +0100

    Faster IsAssemblyCsharp

--- src/XUnity.AutoTranslator.Plugin.Core/Extensions/TypeExtensions.cs
+++ src/XUnity.AutoTranslator.Plugin.Core/Extensions/TypeExtensions.cs
@@ -10,14 +10,27 @@ namespace XUnity.AutoTranslator.Plugin.Core.Extensions
          return Nullable.GetUnderlyingType( type ) ?? type;
       }
 
-      public static bool IsAssemblyCsharp( this Assembly originalAssembly)
+      private static Assembly _assemblyCsharp;
+      public static bool IsAssemblyCsharp( this Assembly assembly )
       {
-         return originalAssembly.GetName().Name.Equals( "Assembly-CSharp" );
+         if( assembly == null ) throw new ArgumentNullException( nameof( assembly ) );
+
+         if( _assemblyCsharp != null )
+            return _assemblyCsharp.Equals( assembly );
+
+         var isAssemblyCsharp = assembly.GetName().Name.Equals( "Assembly-CSharp" );
+
+         if( isAssemblyCsharp )
+            _assemblyCsharp = assembly;
+
+         return isAssemblyCsharp;
       }
 
-      public static bool IsAssemblyCsharpFirstpass( this Assembly originalAssembly )
+      public static bool IsAssemblyCsharpFirstpass( this Assembly assembly )
       {
-         return originalAssembly.GetName().Name.Equals( "Assembly-CSharp-firstpass" );
+         if( assembly == null ) throw new ArgumentNullException( nameof( assembly ) );
+
+         return assembly.GetName().Name.Equals( "Assembly-CSharp-firstpass" );
       }
    }
 }
